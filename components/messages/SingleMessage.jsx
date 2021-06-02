642d5b3fbb3a7cebf17d3123cd170c1843230aac
J3duardo <jesus_69eduardo@hotmail.com>
2021-06-02 01:10:17 -0400

    Componente del mensaje individual del chat con funcionalidad para eliminar el mensaje

---
+++ components/messages/SingleMessage.jsx
@@ -0,0 +1,128 @@
+import {useState, useEffect} from "react";
+import {Comment, Popup, Button, Icon, Header} from "semantic-ui-react";
+import moment from "moment";
+import axios from "axios";
+import styles from "./singleMessage.module.css";
+
+const SingleMessage = ({message, setMessages, currentUser}) => {
+  // Verificar si el usuario actual es el que envía
+  const [isCurrentUserSender, setIsCurrentUserSender] = useState(currentUser === message.sender.username);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setIsCurrentUserSender(currentUser === message.sender.username);
+    setUser(message.sender);
+  }, [message, currentUser]);
+
+  const [deleting, setDeleting] = useState(false);
+  const [errorDeleting, setErrorDeleting] = useState(null);
+
+  /*------------------------------------------------*/
+  // Borrar el mensaje (Cambiar su status a inactive)
+  /*------------------------------------------------*/
+  const deleteMessageHandler = async (msgId) => {
+    try {
+      setDeleting(true);
+      setErrorDeleting(null);
+
+      const res = await axios({
+        method: "PATCH",
+        url: `/api/chats/message/${msgId}`
+      });
+
+      const updatedMessage = {
+        ...res.data.data,
+        text: "Deleted message"
+      }
+
+      // Actualizar el mensaje en la bandeja
+      setMessages(prev => {
+        const allMessages = [...prev];
+        const msgIndex = allMessages.findIndex(el => el._id.toString() === updatedMessage._id.toString());
+        allMessages.splice(msgIndex, 1, updatedMessage);
+        return allMessages;
+      });
+
+      setDeleting(false);
+      
+    } catch (error) {
+      let message = error.message;
+
+      if(error.response) {
+        message = error.response.data.message
+      }
+
+      setErrorDeleting(message);
+      setDeleting(false);
+    }
+  }
+
+  return (
+    <Comment
+      style={{
+        alignSelf: isCurrentUserSender ? "flex-end" : "flex-start",
+        opacity: deleting ? 0.5 : 1,
+        backgroundColor: message.status === "inactive" ? "gainsboro" : "aliceblue"
+      }}
+      className={styles["message"]}
+    >
+      {/* Popup para eliminar el mensaje */}
+      {isCurrentUserSender && message.status === "active" ?
+        <Popup
+          on="click"
+          position="top right"
+          trigger={
+            <Icon
+              style={{
+                position: "absolute",
+                top: "5px",
+                right: 0,
+                cursor: deleting ? "default" : "pointer"
+              }}
+              disabled={deleting}
+              name="times"
+              size="small"
+              color="red"
+              floated="right"
+            />
+          }
+        >
+          <Header as="p" content="Delete message?"/>
+          <Button
+            disabled={deleting}
+            color="red"
+            icon="trash"
+            compact
+            content="Delete"
+            onClick={() => deleteMessageHandler(message._id)}
+          />
+        </Popup>
+        :
+        null
+      }
+
+      {/* Cuerpo del mensaje */}
+      <Comment.Avatar src={user.avatar} as="a" href={`/user/${user.username}`} />
+      <Comment.Content>
+        <Comment.Author as="a" href={`/user/${user.username}`}>
+          {user.name}
+        </Comment.Author>
+        <Comment.Metadata>
+          {moment(message.createdAt).calendar()}
+        </Comment.Metadata>
+        <Comment.Text>
+          <span
+            style={{
+              fontStyle: message.status === "inactive" ? "italic" : "normal",
+              color: message.status === "inactive" ? "grey" : "black"
+            }}
+          >
+            {message.text}
+          </span>
+        </Comment.Text>
+      </Comment.Content>
+    </Comment>
+  )
+}
+
+export default SingleMessage;
