2946e56cd6bc0bd213e3ff757fb347fd13f8848b
J3duardo <jegq.dev.projects@gmail.com>
2022-12-04 23:43:05 -0400

    Backend: Implementar el state global de socket.io en los eventos de notificaciones

--- socket-backend/notificationsEvents.js
+++ socket-backend/notificationsEvents.js
@@ -1,15 +1,15 @@
+const {connectedUsersState} = require("./connectedUsersState");
 
 // Notificar al usuario si tiene notificación de comentario, like o nuevo seguidor
 const notificationReceived = (io, data) => {
   const {userToNotify} = data;
+  const currentUsers = connectedUsersState.getUsers();
 
-  console.log("notificationReceived", userToNotify);
-
-  const recipient = global.users.find(el => el.userId.toString() === userToNotify.toString());
+  const recipient = currentUsers.find(el => el.userId.toString() === userToNotify.toString());
   if(recipient) {
     io.to(recipient.socketId).emit("receivedNotification");
   }
-}
+};
 
 module.exports = {
   notificationReceived
