3d8df028a9176b0053cdc52e2cd2168945844a0f
J3duardo <jegq.dev.projects@gmail.com>
2022-12-05 09:57:36 -0400

    Backend: Implementación del state global de socket.id en los eventos de los chats

--- socket-backend/chatEvents.js
+++ socket-backend/chatEvents.js
@@ -1,9 +1,13 @@
+const {connectedUsersState} = require("./connectedUsersState");
+
 /*---------------------------------------------*/
 // Crear el nuevo chat en el usuario recipiente
 /*---------------------------------------------*/
 const chatCreated = (io, chat) => {
+  const currentUsers = connectedUsersState.getUsers();
   const {messagesWith: {_id}} = chat;
-  const recipient = global.users.find(user => user.userId.toString() === _id.toString());
+
+  const recipient = currentUsers.find(user => user.userId.toString() === _id.toString());
   if(recipient) {
     io.to(recipient.socketId).emit("newChatCreated", chat);
   }
@@ -13,11 +17,12 @@ const chatCreated = (io, chat) => {
 // Notificar que el chat fue deshabilitado
 /*---------------------------------------*/
 const disabledChat = (io, chat) => {
+  const currentUsers = connectedUsersState.getUsers();
   const {user, messagesWith, disabledBy} = chat;
 
   const userToNotify = disabledBy.toString() === user._id.toString() ? messagesWith._id.toString() : user._id.toString();
 
-  const recipient = global.users.find(el => el.userId.toString() === userToNotify);
+  const recipient = currentUsers.find(el => el.userId.toString() === userToNotify);
   if(recipient) {
     io.to(recipient.socketId).emit("chatDisabled", chat);
   }
@@ -28,12 +33,13 @@ const disabledChat = (io, chat) => {
 // Notificar que el chat fue habilitado
 /*-------------------------------------*/
 const enabledChat = (io, data) => {
+  const currentUsers = connectedUsersState.getUsers();
   const {updatedChat, enabledBy} = data;
   const {user, messagesWith} = updatedChat;
 
   const userToNotify = enabledBy.toString() === user._id.toString() ? messagesWith._id.toString() : user._id.toString();
 
-  const recipient = global.users.find(el => el.userId.toString() === userToNotify);
+  const recipient = currentUsers.find(el => el.userId.toString() === userToNotify);
   if(recipient) {
     io.to(recipient.socketId).emit("chatEnabled", updatedChat);
   }
