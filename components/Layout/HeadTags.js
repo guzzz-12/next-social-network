e184a87b55e12654335469dc2abd90820b2d7a85
J3duardo <jegq.dev.projects@gmail.com>
2022-12-20 13:43:28 -0400

    Frontend: Actualizar dinámicamente el title en el Head genérico

--- components/Layout/HeadTags.js
+++ components/Layout/HeadTags.js
@@ -1,20 +1,26 @@
 import Head from "next/head";
+import useUpdateTitleNotifications from "../../hooks/useUpdateTitleNotifications";
 
-const HeadTags = () => (
-  <>
-    <Head>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta charSet="UTF-8" />
-      <link rel="icon" href="/favicon.png" sizes="16*16" type="image/png" />
+const HeadTags = () => {
+  // Actualizar el title de la página según las notificaciones
+  const title = useUpdateTitleNotifications("Next Social Network");
 
-      <link rel="stylesheet" type="text/css" href="/listMessages.css" />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.png" sizes="16*16" type="image/png" />
 
-      <link rel="stylesheet" type="text/css" href="/styles.css" />
-      <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+        <link rel="stylesheet" type="text/css" href="/listMessages.css" />
 
-      <title>Mini Social Media</title>
-    </Head>
-  </>
-);
+        <link rel="stylesheet" type="text/css" href="/styles.css" />
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+
+        <title>{title}</title>
+      </Head>
+    </>
+  )
+};
 
 export default HeadTags;
